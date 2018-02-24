3cbe931cc782e1f8234531795d4dfd1a86b4cc61
“zhaotingtin <zhaotingting@hemaapp.com>
2018-02-24 15:21:54 +0800

    first

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$("img.lazy").lazyload();
+//代码统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?4e78d25d2e8c7e1f2887346af54ba090";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
